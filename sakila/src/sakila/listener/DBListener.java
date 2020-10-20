6378104f8dcd2a072dee304ed6429ba407f6cf86
GDJ29 <GDJ29@DESKTOP-RCG001F>
2020-10-20 11:26:50 +0900

    2020-10-20 오전 11:26 sakila.listener 만듦

---
+++ sakila/src/sakila/listener/DBListener.java
@@ -0,0 +1,29 @@
+package sakila.listener;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+
+
+ 
+@WebListener
+public class DBListener implements ServletContextListener {
+    public DBListener() {
+
+    }
+    public void contextDestroyed(ServletContextEvent arg0)  { 
+
+    }
+    public void contextInitialized(ServletContextEvent arg0)  { 
+        System.out.println("DBListener.contextInitialized() 실행");
+        try {
+        	Class.forName("org.mariadb.jdbc.Driver");
+        	System.out.println("Driver 실행");
+        	
+        }catch(ClassNotFoundException e){
+        	System.out.println("class.forName() 실행 실패 !");
+        	e.printStackTrace();
+        }
+    }
+	
+}
